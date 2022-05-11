29105a317927d19a25bb733d2ffeea645c59f94f
Song jae hyeok <wakij6587@naver.com>
2022-05-11 11:45:10 +0900

    Feat: [id].tsx 비동기 통신 getServerSideProps 적용

--- next-ts/pages/detail/[id].tsx
+++ next-ts/pages/detail/[id].tsx
@@ -1,28 +1,39 @@
 import axios from 'axios';
-import { useRouter } from 'next/router';
-import React, { useCallback, useEffect, useState } from 'react';
+import Head from 'next/head';
+import React from 'react';
 import ItemEl from '../../src/components/itemEl';
-import { IItemEl } from '../../src/types/itemInterface';
+import { IItemType } from '../../src/types/itemInterface';
 
-const Detail = () => {
-  const router = useRouter();
-  const { id } = router.query;
-  const [item, setItem] = useState<IItemEl>();
+interface IDetail {
+  item: IItemType;
+}
 
-  // 아이템 정보
-  const getItemData = useCallback(async (): Promise<void> => {
-    const API_URL = `https://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
-    const itemData: IItemEl = await axios.get(API_URL).then((res: any) => res.data);
-    setItem(itemData);
-  }, [id]);
-
-  useEffect(() => {
-    if (id) {
-      getItemData();
-    }
-  }, [id, getItemData]);
-
-  return <ItemEl item={item} />;
+const Detail = ({ item }: IDetail) => {
+  return (
+    <>
+      {item && (
+        <>
+          <Head>
+            <title>{item.name}</title>
+            <meta name="description" content={item.description}></meta>
+          </Head>
+          <ItemEl item={item} />
+        </>
+      )}
+    </>
+  );
 };
+
+export async function getServerSideProps(context: any) {
+  const id = context.params.id;
+  const API_URL = `https://makeup-api.herokuapp.com/api/v1/products/${id}.json`;
+  const itemData: IItemType = await axios.get(API_URL).then((res: any) => res.data);
+
+  return {
+    props: {
+      item: itemData,
+    },
+  };
+}
 
 export default Detail;
